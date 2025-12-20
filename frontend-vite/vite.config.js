33b10e347f4372eeed895fa1b105143e6a5ab27d
gani-gani <ganeshvg45@gmail.com>
2025-12-20 11:17:29 +0530

    jhbew

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   preview: {
     host: true,
     port: 10000,
-    allowedHosts: ["s-society-1.onrender.com"],
+    allowedHosts: [
+      "kpt-admissions-1.onrender.com"
+    ],
   },
 });
